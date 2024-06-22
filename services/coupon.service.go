a43582e27aa3f107919c72a00f35b2f88c936210
Sappy <sappy5678@gmail.com>
2024-06-22 15:36:47 +0800

    implement time blocker

--- services/coupon.service.go
+++ services/coupon.service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"time"
 
 	db "github.com/ebubekiryigit/golang-mongodb-rest-api-starter/models/db"
@@ -11,8 +12,62 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+type couponConfig struct {
+	CouponProbability int
+
+	ReserveTimeMin time.Time
+	ReserveTimeMax time.Time
+	SnatchTimeMin  time.Time
+	SnatchTimeMax  time.Time
+}
+
+func (c *couponConfig) isBetweenTime(n time.Time, min time.Time, max time.Time) bool {
+	h, m, s := n.Clock()
+	clockStr := fmt.Sprintf("0001-01-01T%d:%d:%dZ", h, m, s)
+	clock, err := time.Parse(time.RFC3339, clockStr)
+	if err != nil {
+		return false
+	}
+	return min.Before(clock) && clock.Before(max)
+}
+
+func (c *couponConfig) isReserveTime(n time.Time) bool {
+	return c.isBetweenTime(n, c.ReserveTimeMin, c.ReserveTimeMax)
+}
+func (c *couponConfig) isSnatchTime(n time.Time) bool {
+	return c.isBetweenTime(n, c.SnatchTimeMin, c.SnatchTimeMax)
+}
+
+func (c *couponConfig) isWinCoupon() bool {
+	return rand.Intn(10000) < c.CouponProbability
+}
+
+// We can get coupon config from DB, but in there we simplify the problem, hardcode in there.
+func getCouponConfig() couponConfig {
+	ReserveTimeMin, _ := time.Parse(time.RFC3339, "0001-01-01T22:55:00Z")
+	ReserveTimeMax, _ := time.Parse(time.RFC3339, "0001-01-01T22:59:00Z")
+	SnatchTimeMin, _ := time.Parse(time.RFC3339, "0001-01-01T23:00:00Z")
+	SnatchTimeMax, _ := time.Parse(time.RFC3339, "0001-01-01T23:01:00Z")
+	return couponConfig{
+		CouponProbability: 2000,
+
+		ReserveTimeMin: ReserveTimeMin,
+		ReserveTimeMax: ReserveTimeMax,
+		SnatchTimeMin:  SnatchTimeMin,
+		SnatchTimeMax:  SnatchTimeMax,
+	}
+}
+
 // Reserve a coupon
 func Reserve(userID string) (*db.Coupon, error) {
+
+	config := getCouponConfig()
+	t := time.Now()
+
+	if !config.isReserveTime(t) {
+		return nil, fmt.Errorf("not reserve time")
+	}
+
 	couponID := uuid.NewString()
 	coupon := db.NewCoupon(couponID, userID, db.CouponStatusNotActive)
 	isReserved, err := checkAndSetReserveUser(userID)
@@ -22,6 +77,7 @@ func Reserve(userID string) (*db.Coupon, error) {
 		err := mgm.Coll(coupon).First(bson.M{"userID": userID}, coupon)
 		return coupon, err
 	}
+
 	err = mgm.Coll(coupon).Create(coupon)
 	if err != nil {
 		return nil, err
@@ -32,6 +88,13 @@ func Reserve(userID string) (*db.Coupon, error) {
 
 // Snatch activate a coupon
 func Snatch(userID string) (*db.Coupon, error) {
+	config := getCouponConfig()
+	t := time.Now()
+
+	if !config.isSnatchTime(t) {
+		return nil, fmt.Errorf("not snatch time")
+	}
+
 	coupon := &db.Coupon{}
 	res := mgm.Coll(coupon).FindOneAndUpdate(context.Background(), bson.M{"userID": userID}, bson.M{"$set": bson.M{"status": db.CouponStatusActive}})
 	if res.Err() != nil {
